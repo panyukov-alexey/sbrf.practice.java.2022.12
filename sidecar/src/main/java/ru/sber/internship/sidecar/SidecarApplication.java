f9eca4220e894a68e97f2af130c400e257325090
16689879 <panyukov.a.ge@sberbank.ru>
2022-12-05 21:22:18 +0600

    initial

---
+++ sidecar/src/main/java/ru/sber/internship/sidecar/SidecarApplication.java
@@ -0,0 +1,13 @@
+package ru.sber.internship.sidecar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SidecarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SidecarApplication.class, args);
+	}
+
+}
---
+++ sidecar/src/test/java/ru/sber/internship/sidecar/SidecarApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sber.internship.sidecar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SidecarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
